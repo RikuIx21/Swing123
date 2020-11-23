94ebd54356c08e34da38dea41c56d30a489a7c62
ZRLAXM <47636825+ZRLAXM@users.noreply.github.com>
2020-11-23 11:04:15 +0800

    Create 1123Random.java

---
+++ 1123Random.java
@@ -0,0 +1,19 @@
+package Kechen.kc20201123;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        while (true){
+            int x = (int) (Math.random() * 1920);
+            int y = (int) (Math.random() * 1080);
+            System.out.println("请输入一个数字：");
+            int i = scan.nextInt();
+            if (i==1){
+                System.out.println("x坐标"+x+" "+"y坐标"+y);
+            }
+        }
+    }
+}
