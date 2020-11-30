9dae020589fb5c522d5836da41b21f63f7ba722e
ZRLAXM <47636825+ZRLAXM@users.noreply.github.com>
2020-11-30 11:42:53 +0800

    Create 1130App_main.java
    main method

---
+++ 1130App_main.java
@@ -0,0 +1,33 @@
+package Kechen.kc20201130;
+
+public class App extends R implements S {
+    public static void main(String[] args) {
+        R r = new R();
+        System.out.println(r.boy_filePathString);
+        System.out.println(r.girl_filePathString);
+        for (int i=0;i<r.arr.length;i++){
+            for (int j=0;j<r.arr[i].length;j++){
+                System.out.print(r.arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        String b = S.boy_filePathString;
+        System.out.println(b);
+        String c = S.girl_filePathString;
+        System.out.println(c);
+        for (int i=0;i<S.arr.length;i++){
+            for (int j=0;j<S.arr[i].length;j++){
+                System.out.print(S.arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+    }
+
+    @Override
+    public void ccc() {
+        System.out.println("hahaha");
+    }
+}
+
