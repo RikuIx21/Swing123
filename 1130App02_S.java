f361d770d9f49772db4e8507fc57d535525c215d
ZRLAXM <47636825+ZRLAXM@users.noreply.github.com>
2020-11-30 11:42:20 +0800

    Create 1130App02_S.java
    S interface

---
+++ 1130App02_S.java
@@ -0,0 +1,8 @@
+package Kechen.kc20201130;
+
+public interface S {
+    String boy_filePathString="111img/boy.gif";
+    String girl_filePathString="111img/girl.gif";
+    int [][] arr= {{1,2,3},{4,5,6}};
+    void ccc();
+}
